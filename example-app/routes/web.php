7a998ae2b4ab30dcff84ab159022624cc65d81f1
nakaji <nakaji.m.0625@gmail.com>
2022-09-08 23:41:05 +0900

    add controllers.

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class, 'show']);
+
+Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class);
