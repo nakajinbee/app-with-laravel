646bd4c567500b47ce9c17ec697012d0d6eba6eb
nakaji <nakaji.m.0625@gmail.com>
2022-09-19 23:47:23 +0900

    edit display descending order

--- example-app/app/Http/Controllers/Tweet/IndexController.php
+++ example-app/app/Http/Controllers/Tweet/IndexController.php
@@ -16,8 +16,7 @@ class IndexController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $tweets = Tweet::all();
-        // dd($tweets);
+        $tweets = Tweet::orderBy('created_at', 'DESC')->get();
         return view('tweet.index')
             ->with('tweets', $tweets);
     }
